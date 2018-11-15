71d78ad3cec469d1dab0b7ff4e299af5e3f53102
Gonzalo Ferrer <g.ferrer@skoltech.ru>
2018-11-15 21:08:11 +0300

    Project on binding SE3 to python, working

--- src/skmrpy/SE3py.cpp
+++ src/skmrpy/SE3py.cpp
@@ -14,8 +14,9 @@
  * using the pybind11 library, here included as an external dependency,
  */
 
-#include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 namespace py = pybind11;
 
 #include "skmr/SE3.hpp"
@@ -24,46 +25,31 @@ using namespace skmr;
 
 
 /**
- * Class to overcome the templated constructor. Not working
+ * Class to overcome the Eigen templated constructor, which does work.
  */
-class PySE3 : public SE3{
+class PySE3 {
   public:
-    PySE3(const Mat61 &xi) : SE3(xi) { };
-    Mat4 T() const {return (Mat4)*this;};
+    PySE3(const Mat61 &xi) : T_(xi) { };
+    Mat4 T() {return (Mat4)T_;};
+    void update(const Mat61 &dxi) {T_.update(dxi);};
+    Mat61 ln() {return T_.ln_vee();};
+
+
+  protected:
+    SE3 T_;
+
 };
 
 
-class MyClass{
-    public:
-    MyClass() : a_(10){};
-    template <typename T> MyClass(T a) : a_(a) {};
-    int get() {return a_;}
-    protected:
-    int a_;
-};
 
-
-PYBIND11_MODULE(skmrpy, m) {
-    m.doc() = "pybind11 SE3 plugin";
-    // Later, in binding code:
-    py::class_<MyClass>(m, "SE3")
-        .def(py::init<int>())
-        .def("get",&MyClass::get)
-        ;
-}
-
-
-/*
 PYBIND11_MODULE(skmrpy, m) {
     m.doc() = "pybind11 SE3 plugin";
     // Later, in binding code:
     py::class_<PySE3>(m, "SE3")
         .def(py::init<const Mat61 &>())
         .def("T", &PySE3::T) // makes a copy of the 4x4 Transformation
-        //.def("update", &SE3::update )
-        //.def("ln_vee", &SE3::ln_vee)
-        //.def("transform", &SE3::transform, py::return_value_policy::reference_internal)
-        //.def("print",&PySE3::print)
+        .def("update", &PySE3::update )
+        .def("ln", &PySE3::ln)
         ;
 }
-*/
+
