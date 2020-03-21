b5bf3e30d5270f6495fc11b290a6827bf9646933
Gonzalo Ferrer <g.ferrer@skoltech.ru>
2020-03-21 18:26:03 +0300

    FGgrap 3dlandmakr example in cpp and python example

---
+++ mrobpy/examples/FGraph_ladmark_3d_example.py
@@ -0,0 +1,40 @@
+#
+# add path to local library mrob on bashr_rc: "export PYTHONPATH=${PYTHONPATH}:${HOME}/mrob/mrob/lib"
+import mrob
+import numpy as np
+
+# example translated from FGrpah/examples/example_solver_3d_landmarks.cpp
+
+graph = mrob.FGraph()
+
+n1 = graph.add_node_pose_3d(np.random.randn(6)*0.05)
+# non-initialized landmarks, nut they could be initialiazed
+l1 = graph.add_node_landmark_3d(np.zeros(3))
+l2 = graph.add_node_landmark_3d(np.zeros(3))
+l3 = graph.add_node_landmark_3d(np.zeros(3))
+print('node pose id = ', n1, ' , node landmark 1 id = ', l1 , ' , node landmark 2 id = ', l2, ' , node landmark 3 id = ', l3)
+
+# anchor factor
+W_0 = np.identity(6)
+graph.add_factor_1pose_3d(np.zeros(6),n1,1e6*W_0)
+
+# landmark factors
+obs = np.array([1,0,0])
+W = np.identity(3)
+# XXX there is a flag (not implemented) for autoinitializaing landmarks (with inverse function). Please tell me if it is necessary/useful
+graph.add_factor_1pose_1landmark_3d(obs,n1,l1,W)
+obs = np.array([1,1,0])
+graph.add_factor_1pose_1landmark_3d(obs,n1,l2,W)
+obs = np.array([1,1,1])
+graph.add_factor_1pose_1landmark_3d(obs,n1,l3,W)
+
+graph.print(True)
+
+
+
+print('\n\n\n Solving Fgraph:\n')
+#graph.solve(mrob.GN) #1 iteration of Gauss-Newton
+graph.solve(mrob.LM) #as many iterations until convergence for Levenberg Marquardt
+graph.print(True)
+
+
