0454ebc762aa08cf0cdb4dca711c38aea6e8dd7f
Gonzalo Ferrer <g.ferrer@skoltech.ru>
2020-07-08 14:32:04 +0300

    Optimier, some structure,. WIP

--- src/common/mrob/optimizer.hpp
+++ src/common/mrob/optimizer.hpp
@@ -12,14 +12,27 @@
 #ifndef OPTIMIZER_HPP_
 #define OPTIMIZER_HPP_
 
+#include "mrob/matrix_base.hpp"
+
+namespace mrob{
+
 /**
  * The class optimizer provides a level of abstraction for solving
  * second order optimization problems of the form:
  *
- * min_x C(X)
+ * min C(x)
  *
- * The current methods implemented are Newton-Raphson (NR) Gauss-Newton (GN) and
- * Levenberg-Marquard (LM)
+ * The current methods implemented are:
+ *  - Newton-Raphson (NR), this is a second order method of the form:
+ *         x' = x - (Hessian)^-1 * Gradient
+ *
+ *         Note that Gauss-Newtow is an instance of this, but approximating the
+ *         Hessian by J'*J
+ *  - Levenberg-Marquard: Spherical approximation
+ *         x' = x - (H + lambda * I)^-1 * gradient
+ *  - Levenberg-Marquard: Elliptical approximation
+ *         x' = x - (H + lambda * D)^-1 * gradient, where D = diag(H)
+ *
  *  given the following requirements:
  *  - C(x): a Cost function
  */
@@ -28,10 +41,29 @@
 class Optimizer
 {
 public:
+	/**
+	 * This enums optimization methods available:
+	 *  - NR: Newton-Raphson (Gauss_newtow is a variant with approximmations in the Hessian)
+	 *  - LM_S: Levenberg Marquardt: Spherical
+	 *  - LM_E: Levenberg Marquardt: Eliptical
+	 */
+	enum optimMethod{NR=0, LM_S, LM_E};
     Optimizer();
-    ~Optimizer();
+    virtual ~Optimizer();
 
-    // TODO how to sperate this optmizers or do a generic class?
+    /**
+     * Once the gradient and Hessian are generated, it optimizes according to
+     * the Newton-Raphson algorithm (second order method).
+     *
+     *
+     * Input useLambda (default false) builds the NR problem with lambda factor on the diagonal
+     *    H' = H + lambda * D. where D depends on which LM has been selected (TODO better)
+     */
+    void optimize_gauss_newton(bool useLambda = false);
+
+    /**
+     * General abstract functions to implement
+     */
     virtual void calculate_error() = 0;
     virtual void calculate_gradient() = 0;
     virtual void calculate_hessian() = 0;
@@ -41,8 +73,18 @@ public:
      * Specific methods for Levenberg-Marquard
      */
 
+protected:
+    optimMethod optimMethod_;
+    MatX hessian_;
+    MatX1 gradient_, dx_;
+    matData_t solutionTolerance_;
+
+    // Necessary for LM
+    MatX1 diag_hessian_;
+    matData_t lambda_;
 
 };
+}
 
 
 #endif /* SRC_COMMON_MROB_OPTIMIZER_HPP_ */
