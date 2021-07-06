9497068fd835c79fae620166ed1c924b1d9adfd6
Gonzalo Ferrer <g.ferrer@skoltech.ru>
2021-07-07 00:34:28 +0300

    adding some robust functions in 3d landmarks

--- python_examples/FGraph_landmark_3d_example.py
+++ python_examples/FGraph_landmark_3d_example.py
@@ -5,9 +5,14 @@ import numpy as np
 
 # example translated from FGrpah/examples/example_solver_3d_landmarks.cpp
 
-graph = mrob.FGraph(mrob.QUADRATIC)
+# Different ways to initialize robust factors
+#graph = mrob.FGraph(mrob.QUADRATIC)
+#graph = mrob.FGraph(mrob.HUBER)
+#graph = mrob.FGraph(mrob.MCCLURE)
+graph = mrob.FGraph(mrob.CAUCHY)
 
-n1 = graph.add_node_pose_3d(mrob.geometry.SE3(np.random.randn(6)*0.05))
+xi = np.array([-0.0282668,  -0.05882867,  0.0945925,  -0.02430618,  0.01794402, -0.06549129])
+n1 = graph.add_node_pose_3d(mrob.geometry.SE3(xi))
 # non-initialized landmarks, nut they could be initialiazed
 l1 = graph.add_node_landmark_3d(np.zeros(3))
 l2 = graph.add_node_landmark_3d(np.zeros(3))
@@ -21,14 +26,13 @@ graph.add_factor_1pose_3d(mrob.geometry.SE3(),n1,1e6*W_0)
 # landmark factors
 obs = np.array([1,0,0])
 W = np.identity(3)
-# XXX there is a flag (not implemented) for autoinitializaing landmarks (with inverse function). Please tell me if it is necessary/useful
 graph.add_factor_1pose_1landmark_3d(obs,n1,l1,W)
 obs = np.array([1,1,0])
 graph.add_factor_1pose_1landmark_3d(obs,n1,l2,W)
 obs = np.array([1,1,1])
 graph.add_factor_1pose_1landmark_3d(obs,n1,l3,W)
 
-graph.print(True)
+#graph.print(True)
 
 
 
@@ -41,8 +45,9 @@ graph.solve(mrob.LM) #as many iterations until convergence for Levenberg Marquar
 
 
 # testing matrix
-import matplotlib.pyplot as plt
-L = graph.get_information_matrix()
-plt.spy(L, marker='o', markersize=5)
-plt.title('Information matrix $\Lambda$')
-plt.show()
+if 0:
+    import matplotlib.pyplot as plt
+    L = graph.get_information_matrix()
+    plt.spy(L, marker='o', markersize=5)
+    plt.title('Information matrix $\Lambda$')
+    plt.show()
