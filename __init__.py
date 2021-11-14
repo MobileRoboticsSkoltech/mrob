2e3e93d2d22c359fe6414696438835950cd9ee92
Lyubov M <miloslubov@gmail.com>
2021-11-14 05:11:53 +0300

    ImportError

--- __init__.py
+++ __init__.py
@@ -30,4 +30,20 @@ try:
 
     del(mrob)
 except ImportError:
-    raise ImportError("You don't have Microsoft Visual C++ installed. Please follow the link and install redistributable package: https://docs.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-160#visual-studio-2015-2017-2019-and-2022") from None
+    import platform, subprocess, ctypes
+    
+    if platform.system() == "Windows":
+        if ctypes.sizeof(ctypes.c_voidp) * 8 > 32:
+            msvc_not_instaled = subprocess.call('REG QUERY "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\VisualStudio\\14.0\\VC\\Runtimes\\x64"', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            msvc_not_instaled = min(msvc_not_instaled, subprocess.call('REG QUERY "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\14.0\\VC\\Runtimes\\x64"', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL))
+        else:
+            msvc_not_instaled = subprocess.call('REG QUERY "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\VisualStudio\\14.0\\VC\\Runtimes\\x86"', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            msvc_not_instaled = min(msvc_not_instaled, subprocess.call('REG QUERY "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\14.0\\VC\\Runtimes\\x86"', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL))
+
+    if msvc_not_instaled:
+        import sys
+        
+        sys.tracebacklimit = 0
+        raise ImportError("You don't have Microsoft Visual C++ installed. Please follow the link and install redistributable package: https://docs.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-160#visual-studio-2015-2017-2019-and-2022") from None
+        
+    del(platform, subprocess, ctypes, msvc_not_instaled)
